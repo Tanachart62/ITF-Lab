235e228801ff61e68f84a4d1732ef3b2183ec2fa
Awesome <67070062@kmitl.ac.th>
2025-05-17 18:53:53 +0700

    ไม่มีอะไร
    กก

--- Hello.py
+++ Hello.py
@@ -1,13 +1,1 @@
 print("hello word")
-x = int(input())
-for i in range(x):
-    for j in range(i):
-        for k in range(j*2):
-            print("gjgghgj")
-        print("*")
-        print(123454                                          )
-        # # เวกบลเสยบกสเกเสวกบยเ
-        # ้่เ่ะ
-        # ้าเ้ั่เ่เัั
-        
-        l'l'l'l'
